1b688d4740b1b93a7ad8ebdce88cb4f78b9b2379
shubhanshubb <shubhanshubb@gmail.com>
2025-06-08 15:34:27 +0530

    Initial Commit

---
+++ SampleApp/SampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAppApp.swift
+//  SampleApp
+//
+//  Created by Shubhanshu Kumar on 08/06/25.
+//
+
+import SwiftUI
+
+@main
+struct SampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
